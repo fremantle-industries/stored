49359534eec8b2f5aadae4d3810be0702282aa28
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-05-09 16:01:31 -0700

    Create ETS backend specific tests

--- lib/stored/store.ex
+++ lib/stored/store.ex
@@ -7,8 +7,8 @@ defmodule Stored.Store do
   @callback after_clear() :: no_return
   @optional_callbacks after_backend_create: 0, backend_created: 0, after_put: 1, after_clear: 0
 
-  defmacro __using__(_) do
-    quote location: :keep do
+  defmacro __using__(opts \\ []) do
+    quote bind_quoted: [opts: opts], location: :keep do
       use GenServer
 
       @behaviour Stored.Store
@@ -18,18 +18,17 @@ defmodule Stored.Store do
       @type key :: Stored.Backend.key()
 
       @default_id :default
-      @default_backend Stored.Backends.ETS
+      @backend Keyword.get(opts, :backend, Stored.Backends.ETS)
 
       defmodule State do
-        @type t :: %State{id: atom, name: atom, backend: module}
-        defstruct ~w(id name backend)a
+        @type t :: %State{id: atom, name: atom}
+        defstruct ~w(id name)a
       end
 
       def start_link(args) do
         id = Keyword.get(args, :id, @default_id)
-        backend = Keyword.get(args, :backend, @default_backend)
         name = process_name(id)
-        state = %State{id: id, name: name, backend: backend}
+        state = %State{id: id, name: name}
 
         GenServer.start_link(__MODULE__, state, name: name)
       end
@@ -73,30 +72,30 @@ defmodule Stored.Store do
       def init(state), do: {:ok, state, {:continue, :init}}
 
       def handle_continue(:init, state) do
-        :ok = state.backend.create(state.name)
+        :ok = @backend.create(state.name)
         after_backend_create()
         backend_created()
         {:noreply, state}
       end
 
       def handle_call({:put, record}, _from, state) do
-        response = state.backend.put(record, state.name)
+        response = @backend.put(record, state.name)
         after_put(record)
         {:reply, response, state}
       end
 
       def handle_call({:find, key}, _from, state) do
-        response = state.backend.find(key, state.name)
+        response = @backend.find(key, state.name)
         {:reply, response, state}
       end
 
       def handle_call(:all, _from, state) do
-        response = state.backend.all(state.name)
+        response = @backend.all(state.name)
         {:reply, response, state}
       end
 
       def handle_call(:clear, _from, state) do
-        response = state.backend.clear(state.name)
+        response = @backend.clear(state.name)
         after_clear()
         {:reply, response, state}
       end
--- test/stored/store_test.exs
+++ test/stored/store_test.exs
@@ -5,10 +5,6 @@ defmodule Stored.StoreTest do
 
   defmodule TestStore do
     use Stored.Store
-  end
-
-  defmodule TestStoreWithCallbacks do
-    use Stored.Store
 
     def after_backend_create do
       send(Stored.StoreTest, :after_backend_create)
@@ -23,6 +19,11 @@ defmodule Stored.StoreTest do
     end
   end
 
+  setup do
+    Process.register(self(), @test_store_id)
+    :ok
+  end
+
   test "can start multiple stores" do
     assert {:ok, pid_a} = TestStore.start_link(id: :"#{@test_store_id}_a")
     assert {:ok, pid_b} = TestStore.start_link(id: :"#{@test_store_id}_b")
@@ -31,86 +32,24 @@ defmodule Stored.StoreTest do
   end
 
   test "fires callback 'after_backend_create/0'" do
-    Process.register(self(), @test_store_id)
-
-    start_supervised!({TestStoreWithCallbacks, id: @test_store_id})
+    start_supervised!({TestStore, id: @test_store_id})
 
     assert_receive :after_backend_create
   end
 
-  describe ".put/1" do
-    test "can inject a record" do
-      lebron = %TestSupport.Person{first_name: "Lebron", last_name: "James"}
-      mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
-      start_supervised({TestStore, id: @test_store_id})
+  test ".put/1 fires callback 'after_put/1'" do
+    start_supervised!({TestStore, id: @test_store_id})
 
-      assert {:ok, {u_lebron_key, u_lebron}} = TestStore.put(lebron, @test_store_id)
-      assert u_lebron_key == "Lebron_James"
-      assert u_lebron == lebron
+    lebron = %TestSupport.Person{first_name: "Lebron", last_name: "James"}
 
-      assert {:ok, {u_mj_key, u_mj}} = TestStore.put(mj, @test_store_id)
-      assert u_mj_key == "Michael_Jordan"
-      assert u_mj == mj
-    end
-
-    test "fires callback 'after_put/1'" do
-      Process.register(self(), @test_store_id)
-      start_supervised!({TestStoreWithCallbacks, id: @test_store_id})
-
-      lebron = %TestSupport.Person{first_name: "Lebron", last_name: "James"}
-
-      assert {:ok, {u_lebron_key, u_lebron}} = TestStoreWithCallbacks.put(lebron, @test_store_id)
-      assert_receive {:after_put, put_record}
-    end
+    assert {:ok, {u_lebron_key, u_lebron}} = TestStore.put(lebron, @test_store_id)
+    assert_receive {:after_put, put_record}
   end
 
-  test "can find a record by key" do
-    start_supervised({TestStore, id: @test_store_id})
-    mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
-    TestStore.put(mj, @test_store_id)
+  test ".clear/0 fires callback 'after_clear/0'" do
+    start_supervised!({TestStore, id: @test_store_id})
 
-    assert {:ok, found_record} = TestStore.find("Michael_Jordan", @test_store_id)
-    assert found_record == mj
-
-    assert TestStore.find("Lebron_James", @test_store_id) == {:error, :not_found}
-  end
-
-  test "can get all records" do
-    start_supervised({TestStore, id: @test_store_id})
-
-    assert TestStore.all(@test_store_id) == []
-
-    mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
-    assert {:ok, _} = TestStore.put(mj, @test_store_id)
-
-    records = TestStore.all(@test_store_id)
-    assert Enum.count(records) == 1
-    assert Enum.at(records, 0) == mj
-  end
-
-  describe ".clear/0" do
-    test "can clear all records" do
-      start_supervised({TestStore, id: @test_store_id})
-
-      assert TestStore.all(@test_store_id) == []
-
-      mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
-      assert {:ok, _} = TestStore.put(mj, @test_store_id)
-
-      records = TestStore.all(@test_store_id)
-      assert Enum.count(records) == 1
-
-      assert TestStore.clear(@test_store_id) == :ok
-      records = TestStore.all(@test_store_id)
-      assert Enum.count(records) == 0
-    end
-
-    test "fires callback 'after_clear/0'" do
-      Process.register(self(), @test_store_id)
-      start_supervised!({TestStoreWithCallbacks, id: @test_store_id})
-
-      assert TestStoreWithCallbacks.clear(@test_store_id) == :ok
-      assert_receive :after_clear
-    end
+    assert TestStore.clear(@test_store_id) == :ok
+    assert_receive :after_clear
   end
 end
