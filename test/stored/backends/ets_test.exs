b2edd1b0b3bd8dc529e55b6b7a720f965c716059
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-05-30 16:38:50 -0700

    Add count to store

--- test/stored/backends/ets_test.exs
+++ test/stored/backends/ets_test.exs
@@ -2,6 +2,7 @@ defmodule Stored.Backends.ETSTest do
   use ExUnit.Case, async: true
 
   @test_store_id __MODULE__
+  @mj %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
 
   defmodule TestStore do
     use Stored.Store, backend: Stored.Backends.ETS
@@ -15,31 +16,28 @@ defmodule Stored.Backends.ETSTest do
   describe ".put/1" do
     test "can inject a record into the backend" do
       process_name = TestStore.process_name(@test_store_id)
-      mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
 
-      assert {:ok, _} = TestStore.put(mj, @test_store_id)
+      assert {:ok, _} = TestStore.put(@mj, @test_store_id)
 
       records = :ets.lookup(process_name, "Michael_Jordan")
       assert Enum.count(records) == 1
-      assert Enum.at(records, 0) == {"Michael_Jordan", mj}
+      assert Enum.at(records, 0) == {"Michael_Jordan", @mj}
     end
 
     test "returns the key & record" do
-      mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
       start_supervised({TestStore, id: @test_store_id})
 
-      assert {:ok, {u_mj_key, u_mj}} = TestStore.put(mj, @test_store_id)
+      assert {:ok, {u_mj_key, u_mj}} = TestStore.put(@mj, @test_store_id)
       assert u_mj_key == "Michael_Jordan"
-      assert u_mj == mj
+      assert u_mj == @mj
     end
   end
 
   test "can find a record by key" do
-    mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
-    TestStore.put(mj, @test_store_id)
+    TestStore.put(@mj, @test_store_id)
 
     assert {:ok, found_record} = TestStore.find("Michael_Jordan", @test_store_id)
-    assert found_record == mj
+    assert found_record == @mj
 
     assert TestStore.find("Lebron_James", @test_store_id) == {:error, :not_found}
   end
@@ -47,17 +45,14 @@ defmodule Stored.Backends.ETSTest do
   test "can get all records" do
     assert TestStore.all(@test_store_id) == []
 
-    mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
-    assert {:ok, _} = TestStore.put(mj, @test_store_id)
+    assert {:ok, _} = TestStore.put(@mj, @test_store_id)
 
     records = TestStore.all(@test_store_id)
     assert Enum.count(records) == 1
-    assert Enum.at(records, 0) == mj
+    assert Enum.at(records, 0) == @mj
   end
 
   describe ".delete/1" do
-    @mj %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
-
     setup do
       TestStore.put(@mj, @test_store_id)
 
@@ -76,11 +71,16 @@ defmodule Stored.Backends.ETSTest do
     end
   end
 
+  test ".count returns the number of records in the store" do
+    assert TestStore.count(@test_store_id) == 0
+
+    assert {:ok, _} = TestStore.put(@mj, @test_store_id)
+    assert TestStore.count(@test_store_id) == 1
+  end
+
   test "can clear all records" do
     assert TestStore.all(@test_store_id) == []
-
-    mj = %TestSupport.Person{first_name: "Michael", last_name: "Jordan"}
-    assert {:ok, _} = TestStore.put(mj, @test_store_id)
+    assert {:ok, _} = TestStore.put(@mj, @test_store_id)
 
     records = TestStore.all(@test_store_id)
     assert Enum.count(records) == 1
